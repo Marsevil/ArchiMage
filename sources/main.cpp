fd56a063920df02aad4390cb9e3097468606a8ef
Marsevil <marsevil@hotmail.fr>
2020-03-19 11:49:05 +0100

    Ajout main Pour les tests

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include "../headers/DrawableObject.hpp"
+
 int main(int argc, char const *argv[]) {
 	std::cout << "Hello World !!" << std::endl;
-	
+
 	return EXIT_SUCCESS;
 }
