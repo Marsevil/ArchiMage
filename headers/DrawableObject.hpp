f44ceeb5aa8c0a57c22a4160cf8182b6a11e0601
Marsevil <marsevil@hotmail.fr>
2020-03-19 11:24:05 +0100

    Class Drawable Pour dessiner les objet à l'écran

---
+++ headers/DrawableObject.hpp
@@ -0,0 +1,15 @@
+#ifndef DRAWABLE_OBJECT_HPP
+#define DRAWABLE_OBJECT_HPP
+
+#include "../include/lib_objectsReader/objetsOFF.h"
+#include "Drawable.hpp"
+
+class DrawableObject : public Objet, public Drawable {
+public:
+	DrawableObject();
+
+	virtual void draw() const;
+	virtual void constructVBO() const;
+};
+
+#endif /* end of include guard: DRAWABLE_OBJECT_HPP */
